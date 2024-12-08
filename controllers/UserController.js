edb7065cceb28a7f0d4ac82054bf7e857ebaed21
ahmadbagwi <ahmadbagwi.id@gmail.com>
2024-12-08 22:11:35 +0700

    add encrypt and decrypt password

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -5,6 +5,9 @@ const prisma = new PrismaClient()
 const jwt = require("jsonwebtoken")
 const { validationResult } = require("express-validator")
 const { checkToken } = require('../utils/checkToken')
+const bcrypt = require('bcrypt')
+const saltRounds = 10
+const salt = bcrypt.genSaltSync(saltRounds)
 
 const findUsers = async (req, res) => {
   try {
@@ -54,26 +57,32 @@ const createUser = async (req, res) => {
   try {
     //insert data
     if (!existingUser) {
+      // let hashed = ''
+      // bcrypt.genSalt(saltRounds, function (err, salt) {
+      //   bcrypt.hash(myPlaintextPassword, salt, function (err, hash) {
+      //     hashed = hash
+      //   })
+      // })
+      // const hashedPassword = bcrypt.hashSync(req.body.password, salt)
       const user = await prisma.user.create({
         data: {
           name: req.body.name,
           email: req.body.email,
-          password: req.body.password,
+          password: bcrypt.hashSync(req.body.password, salt),
           phone: req.body.phone,
-          address: req.body.address,
-          country: req.body.country
-        },
+          address: req.body.address
+        }
       })
       res.status(201).send({
         success: true,
         message: "User Created Successfully",
-        data: user,
+        data: user
       })
     } else {
       res.status(500).send({
         success: true,
         message: "User Exist",
-        data: user,
+        data: existingUser
       })
     }
   } catch (error) {
@@ -99,11 +108,11 @@ const loginUser = async (req, res) => {
 
   let existingUser = await findUserByEmail(req.body.email)
   
-  if (!existingUser || existingUser.password != req.body.password) {
-    res.status(500).send({
+  if (!existingUser || !bcrypt.compareSync(req.body.password, existingUser.password)) {
+    res.status(401).send({
       success: false,
       message: "email or password didn't match",
-      data: existingUser
+      data: null
     })
   }
 
@@ -150,7 +159,7 @@ const findUserByEmail = async (email) => {
     })
     return user
   } catch (error) {
-    return error
+    return false
   }
 }
 
