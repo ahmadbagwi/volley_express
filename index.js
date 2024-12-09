918a4951247783511474f7c98a2523450ad2950d
nu_vandalstik <wisnukristanto321@gmail.com>
2024-12-09 13:23:02 +0700

    feat-cors option

--- index.js
+++ index.js
@@ -1,33 +1,38 @@
 //import express
-const express = require('express')
+const express = require("express");
 
 //import router
-const router = require('./routes')
+const router = require("./routes");
+const cors = require("cors");
 
 //import bodyParser
-const bodyParser = require('body-parser')
+const bodyParser = require("body-parser");
 
 //init app
-const app = express()
+const app = express();
 
 //define port
-const port = process.env.PORT
+const port = process.env.PORT;
+
+let corsOptions = {
+  origin: "*",
+};
 
 //use body parser
-app.use(bodyParser.urlencoded({ extended: false }))
-
+app.use(cors(corsOptions));
+app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
 //route
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 //define routes
-app.use('/api', router)
+app.use("/api", router);
 
 //start server
 app.listen(port, () => {
-    console.log(`Server started on port ${port}`)
-})
+  console.log(`Server started on port ${port}`);
+});
